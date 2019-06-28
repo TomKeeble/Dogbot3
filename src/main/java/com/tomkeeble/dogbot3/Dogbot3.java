54520b9592b2cd90b9e3a28945e2b25c0fbdb644
Tom Keeble <thomasdkeeble@gmail.com>
2019-06-28 15:47:41 +0100

    Create singletons and add resources directory

--- src/main/java/com/tomkeeble/dogbot3/Dogbot3.java
+++ src/main/java/com/tomkeeble/dogbot3/Dogbot3.java
@@ -1,8 +1,16 @@
 package com.tomkeeble.dogbot3;
 
 import com.rabbitmq.client.ConnectionFactory;
+import com.tomkeeble.dogbot3.messageproviders.facebook.FacebookMessageProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.persistence.EntityManager;
 
 public class Dogbot3 {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
 
     public static MessageProvider getMessageProvider() {
         return messageProvider;
@@ -10,7 +18,23 @@ public class Dogbot3 {
 
     private static MessageProvider messageProvider;
 
+    public static EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    private static EntityManager entityManager;
+
     private void connectMQ(){
         ConnectionFactory factory = new ConnectionFactory();
+    }
+
+    public Dogbot3(EntityManager em) {
+        entityManager = em;
+        messageProvider = new FacebookMessageProvider();
+    }
+
+    public void start(){
+        logger.info("Dogbot3");
+        logger.info("Starting Dogbot3...");
     }
 }
--- src/main/java/com/tomkeeble/dogbot3/Main.java
+++ src/main/java/com/tomkeeble/dogbot3/Main.java
@@ -1,17 +1,26 @@
 package com.tomkeeble.dogbot3;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Dogbot3");
+
+        logger.info("creating persistance manager");
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+        Dogbot3 dogbot = new Dogbot3(entityManager);
+        dogbot.start();
 
     }
 }
