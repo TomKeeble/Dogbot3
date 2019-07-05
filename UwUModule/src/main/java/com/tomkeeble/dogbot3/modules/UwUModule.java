13f1941ce8633a193566a5393455df13a6a591d6
SpruceMarcy <mdr.brook@gmail.com>
2019-07-05 23:43:34 +0100

    Refined UwUification and added :3 mode

--- UwUModule/src/main/java/com/tomkeeble/dogbot3/modules/UwUModule.java
+++ UwUModule/src/main/java/com/tomkeeble/dogbot3/modules/UwUModule.java
@@ -21,6 +21,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Random;
 
 @Stateful
 @Named("UwUModule")
@@ -42,18 +43,17 @@ public class UwUModule implements Module {
         System.out.println("Message!");
 //        message.getThread().sendMessage(msg_provider, new Message("Hello World from " + this.getClassName()));
         if (message.getMessage().toLowerCase().startsWith("!uwu")) {
-            String uwuText = null;
+            String uwuText = message.getMessage().toLowerCase();
             int n = 1;
-
-            try {
-                n = Integer.parseInt(message.getMessage().toLowerCase().substring(5));
-            } catch (NumberFormatException e) {
-                message.getThread().sendMessage(msg_provider, new Message(e.getMessage()));
-            } catch (StringIndexOutOfBoundsException e) {
-
+            boolean faced=uwuText.matches(".*:3.*");
+            if(uwuText.matches("!uwu(:3)? *[+-]?[0-9]+.*")) {
+                try {
+                    n = Integer.parseInt(uwuText.replaceAll("!uwu(:3)? *([+-]?[0-9]+).*","$2"));
+                } catch (Exception e) {
+                    message.getThread().sendMessage(msg_provider, new Message(e.getMessage()));
+                }
             }
-
-            if ( n<=0) {
+            if (n<=0) {
                 n = 1;
             }
             try {
@@ -66,9 +66,19 @@ public class UwUModule implements Module {
             uwuText=uwuText.replaceAll("[RL]","W");
             uwuText=uwuText.replaceAll("(?i)youw","ur");
             uwuText=uwuText.replaceAll("(?i)you","u");
-            uwuText=uwuText.replaceAll("(?i)awe ","r ");
+            uwuText=uwuText.replaceAll("(?i)awe(?![a-z])","r");
             uwuText=uwuText.replaceAll("(?i)ove","uv");
             uwuText=uwuText.replaceAll("(?i)(n)([aeiou])","$1y$2");
+
+            if(faced) {
+                String[] faces={":3","(^³^)","uwu","OwO",":////",";-;;;",">.<"};
+                int facecount=Math.max(1,(int)(Math.round(uwuText.length()/10)*Math.random()));
+                for(int i=facecount;i>0; i--){
+                    int pivot=new Random().nextInt(uwuText.length());
+                    uwuText=uwuText.substring(0,pivot)+(uwuText.substring(pivot)+" ").replaceFirst(" "," "+faces[new Random().nextInt(faces.length)]+" ");
+                }
+            }
+
             message.getThread().sendMessage(msg_provider, new Message(uwuText));
         }
     }
